905f2e8e584bb4bdab118c736a1846592afe3210
ManishKhoday <khodaymanish@gmail.com>
2025-01-26 18:09:25 +0530

    add the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull manish653/simple-python-flask-app:latest
+docker pull manish653/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 manish653/simple-python-flask-app:latest
+docker run -d -p 5000:5000 manish653/simple-python-flask-app
